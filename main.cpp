399b3e158a1b6a15456d5f8755ba4beb1d18437c
Isa Ako <isa.achsanu354@gmail.com>
2019-11-26 16:19:37 +0700

    revisi langkah algoritma

--- main.cpp
+++ main.cpp
@@ -24,11 +24,11 @@ bool is_vector_give_no_cycle(Edge ed, string vect){
     ( ed.v2 == vect && ed.v1 != vect );
 }
 
-bool push_to_vector_selected(Edge ed, vector<string> &vector_selected, int selected_idx){
-    if( vector_selected[selected_idx] != ed.v1 )
-        vector_selected.push_back( ed.v1 );
+bool push_to_vertex_selected(Edge ed, vector<string> &vertex_selected, int selected_idx){
+    if( vertex_selected[selected_idx] != ed.v1 )
+        vertex_selected.push_back( ed.v1 );
     else
-        vector_selected.push_back( ed.v2 );
+        vertex_selected.push_back( ed.v2 );
 }
 
 void output_prim_result(vector<Edge> prim_result){
@@ -45,22 +45,26 @@ int main()
     /// Deklarasi awal
     vector<Edge> T;
     vector<Edge> prim_result;
-    vector<string> vector_selected;
-    int jumlah_vektor;
+    vector<string> vertex_selected;
+    int jumlah_vertex;
+
+    vector<string> list_vertex;
 
     /** start: untuk membaca file .txt, gak usah dipikir */
     std::ifstream file("input.txt");
     std::string str;
-    int first = 1;
+    int first = 2;
     while (std::getline(file, str)) {
         istringstream csvStream(str);
         Edge ins;
         string stri;
         int co = 1;
         while( getline(csvStream, stri, ',') ){
-            if(first==1){
+            if(first==2){
                 stringstream strtoints(stri);
-                if( co==2 ) strtoints >> jumlah_vektor;
+                if( co==2 ) strtoints >> jumlah_vertex;
+            }else if(first==1){
+                list_vertex.push_back( stri );
             }else{
                 if( co==1 ) ins.v1 = stri;
                 if( co==2 ) ins.v2 = stri;
@@ -73,14 +77,12 @@ int main()
             }
             co++;
         }
-        if(first!=1) T.push_back(ins);
-        else first=0;
+        if(first==0) T.push_back(ins);
+        else first--;
     }
     /** end: untuk membaca file .txt, gak usah dipikir */
 
-    /// Ascending Sort
-    sort(T.begin(), T.end(), compareBobot);
-    cout << "Sorted Edge :" << endl;
+    cout << "List Edge :" << endl;
     for(int i=0; i<T.size(); i++){
         cout << T[i].v1 << " " <<  T[i].v2 << " " << T[i].bobot << endl;
     }
@@ -89,14 +91,16 @@ int main()
 
     /** Start Algoritma Prim */
 
-    /// Insert Edge Terkecil
-    prim_result.push_back( T[0] );
-    vector_selected.push_back( T[0].v1 );
-    vector_selected.push_back( T[0].v2 );
-    output_prim_result(prim_result);
+    /// Insert Vertex yg ditentukan
+    string firstVertex = "C";
+    vertex_selected.push_back( firstVertex );
 
-    /// Loop sebanyak jumlah_vektor - 2
-    for(int o=0; o<jumlah_vektor-2; o++){
+    /// Loop sebanyak jumlah_vertex - 1
+    for(int o=0; o<jumlah_vertex-1; o++){
+        int decide_to_insert = 0;
+        int check_vertex_idx = -1;
+        int bobot = 999;
+        Edge insert_edge;
 
         /// Loop untuk mencari edge
         for(int i=0; i<T.size(); i++){
@@ -104,24 +108,26 @@ int main()
             int selected_idx = -1;
 
             /// Loop untuk mengecek vector yg sudah masuk dalam spanning tree
-            for(int j=0; j<vector_selected.size(); j++){
-                if( is_vector_give_no_cycle(T[i], vector_selected[j]) ){
+            for(int j=0; j<vertex_selected.size(); j++){
+                if( is_vector_give_no_cycle(T[i], vertex_selected[j]) ){
                     selected++;
                     selected_idx = j;
                 }
             }
 
-            /// Jika vector memenuhi kriteria
-            if( selected==1 ){
-                push_to_vector_selected( T[i], vector_selected, selected_idx );
-                prim_result.push_back(T[i]);
-
-                output_prim_result(prim_result);
-                break;
-                /** langsung break loop karena sebelumnya T sudah diurutkan,
-                  * jadi yg paling pertama ditemukan pasti yg terkecil
-                  */
+            /// Jika vector memenuhi kriteria, pilih bobot yg lebih kecil
+            if( selected==1 && T[i].bobot<bobot ){
+                bobot = T[i].bobot;
+                insert_edge = T[i];
+                check_vertex_idx = selected_idx;
+                decide_to_insert = 1;
             }
+        }
+        /// masukkan edge ke prim_result
+        if( decide_to_insert==1 ){
+            push_to_vertex_selected( insert_edge, vertex_selected, check_vertex_idx );
+            prim_result.push_back( insert_edge );
+            output_prim_result( prim_result );
         }
 
     }
@@ -129,8 +135,8 @@ int main()
     /** End Algoritma Prim */
 
     cout << "Vektor yg dilewati : ";
-    for(int i=0; i<vector_selected.size(); i++){
-        cout << vector_selected[i] << ",";
+    for(int i=0; i<vertex_selected.size(); i++){
+        cout << vertex_selected[i] << ",";
     }
 
     cout << endl;
